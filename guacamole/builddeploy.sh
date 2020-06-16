a5fe082d90f67819622a352cd62ab593d751b00e
Victor Norman <vtn2@calvin.edu>
2020-06-16 10:31:08 -0400

    fix home.html, etc.

--- guacamole/builddeploy.sh
+++ guacamole/builddeploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Script to build the maven project then copy the resulting war file to the webapps directory.
 
+# Skip checking every blasted file for the license at the top...
 cd /home/Agora/guacamole-client/guacamole
-mvn package
+mvn -Drat.skip=true package
 cd target
 
 # Now copy the war file with an appropriate name to the tomcat webapps folder
